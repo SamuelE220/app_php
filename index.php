8989e6c8841a7e324b1d4285299a8e3d27bbea71
SamuelE220 <145207661+SamuelE220@users.noreply.github.com>
2025-10-31 12:22:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
                 <?php
                 // Procesar el formulario cuando se envía
                 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                    $url = 'http://84.247.188.87/api/clientes';
+                    $url = 'http://84.247.188.87:8090/api/clientes';
                     
                     // Recoger y sanitizar datos del formulario
                     $data = [
